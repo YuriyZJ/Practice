4b6096bc02ba9ccdc411a144f67cfb028ad59e6b
YurıyZJ <yuriy.work93@gmail.com>
2025-03-24 18:43:26 +0300

    Level2 (Task2 (change decision))

---
+++ src/Level2/Task2/Main.java
@@ -0,0 +1,14 @@
+package Level2.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        TextProcessor textProcessor = new TextProcessor();
+
+        textProcessor.processText("Hello World. How mach words in this sentence?");
+        textProcessor.processText("     ");
+
+        TextProcessor textProcessor2 = new TextProcessor();
+        textProcessor2.processText("Hello World. How mach words in this sentence?");
+        textProcessor2.processText("     ");
+    }
+}
--- src/Level2/Task2/TextProcessor.java
+++ src/Level2/Task2/TextProcessor.java
@@ -2,22 +2,37 @@ package Level2.Task2;
 
 public class TextProcessor {
     public void processText(String text){
-        class WordCounter {
-            WordCounter wordCounter = new WordCounter();
 
+        class WordCounter { // локальный класс
             public int countWords(String text){
-                int left = 0;
-                int right = text.length() - 1;
-                int count = 0;
-
-                while (left < right){
-                    if (text.charAt(left) == " "){
+                int first = 0;
+                int count = 1;
+                while (first < text.length()){
+                    if (text == null || text.isBlank()) {
+                        return 0;
+                    } if (text.charAt(first) == ' ') {
                         count++;
                     }
-                    left++;
+                    first++;
                 }
                 return count;
             }
         }
+
+        WordCounter wordCounter = new WordCounter();
+        System.out.println(wordCounter.countWords(text));
+    }
+
+    public void processText2(String text){
+
+        class WordCounter2 {
+            public int countWords(String text){
+                String[] words = text.split(" ");
+                int count = words.length;
+                return count;
+            }
+        }
+        WordCounter2 wordCounter = new WordCounter2();
+        System.out.println(wordCounter.countWords(text));
     }
 }
