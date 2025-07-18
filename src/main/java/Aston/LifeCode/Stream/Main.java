d0be0d508fac6bfb644f4a1f1b61b366b8ffbabe
YurıyZJ <yuriy.work93@gmail.com>
2025-07-18 10:03:45 +0300

    LifeCode(Task1(Ch2))

--- src/main/java/Aston/LifeCode/Stream/Main.java
+++ src/main/java/Aston/LifeCode/Stream/Main.java
@@ -5,19 +5,44 @@ import java.util.List;
 public class Main {
     public static void main(String[] args){
         List<Integer> listN = List.of(1,2,3,4,1,100,2);
-        List<String> listS = List.of("apple", "banana", "kiwi");
+        List<String> listS = List.of("carrot", "apple", "banana", "kiwi");
+        List<Person> list = List.of(
+                new Person("Alice", 17),
+                new Person("Bob", 22),
+                new Person("Charlie", 19),
+                new Person("Dave", 25)
+        );
+        List<List<String>> listCh = List.of(
+                List.of("apple", "banana"),
+                List.of("carrot", "date"),
+                List.of("eggplant")
+        );
+
 
         Solution solution = new Solution();
-        Solution.filter(listN);
-        System.out.println();
+        System.out.println("Solution 1");
+        Solution.filter1(listN);
 
-        Solution.length(listS);
-        System.out.println();
+        System.out.println("Solution 2");
+        Solution.length2(listS);
 
-        //
+        System.out.println("Solution 3");
+        Solution.sum3(listN);
 
-        Solution.sort(listN);
-        System.out.println();
+        System.out.println("Solution 4");
+        Solution.sort4(listN);
+
+        System.out.println("Solution 5");
+        Solution.findFirstWord5(listS);
+
+        System.out.println("Solution 6");
+        Solution.sortAge6(list);
+
+        System.out.println("Solution 7");
+        Solution.change7(listCh);
+
+        System.out.println("Solution 10");
+        Solution.findFirstFreeAliment10(listN);
     }
 }
 
--- src/main/java/Aston/LifeCode/Stream/Solution.java
+++ src/main/java/Aston/LifeCode/Stream/Solution.java
@@ -2,32 +2,66 @@ package Aston.LifeCode.Stream;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
     // Из списка List<Integer> получить список только четных чисел.
-    public static void filter(List<Integer> listF){
+    public static void filter1(List<Integer> listF){
         listF.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
     }
 
     // Преобразовать List<String> в List<Integer>, содержащий длины строк.
-    public static void length(List<String> listL){
+    public static void length2(List<String> listL){
         listL.stream().map(x -> x.length()).forEach(System.out::println);
     }
 
-    /*// Найти сумму всех элементов в списке List<Integer>.
-    public static void sum(List<String> listS){
-        listS.stream().reduce(0, Integer::sum).forEach(System.out::println); //???
-    }*/
+    // Найти сумму всех элементов в списке List<Integer>.
+    public static void sum3(List<Integer> listS){
+        int result = listS.stream().reduce(0, Integer::sum);
+        System.out.println(result);
+    }
 
-    //Из List<String> получить отсортированный список уникальных значений.
-    public static void sort(List<Integer> listS){
+    // Из List<String> получить отсортированный список уникальных значений.
+    public static void sort4(List<Integer> listS){
         listS.stream().sorted(Comparator.reverseOrder()).distinct().forEach(System.out::println);
     }
 
-    //Из List<String> найти первое слово, начинающееся на букву "A" (или вернуть null/Optional.empty()).
-    public static void findFirstWord(List<String> listF){
-        listF.stream().filter(x -> ).
+    // Из List<String> найти первое слово, начинающееся на букву "A" (или вернуть null/Optional.empty()).
+    public static void findFirstWord5(List<String> listF){
+        String result = listF.stream()
+                .filter(x -> x.startsWith("a"))
+                .findFirst().orElse(null);
+        System.out.println(result);
     }
 
+    // Есть список объектов Person(name, age) — получить список имен людей старше 18 лет, отсортированных по возрасту.
+    public static void sortAge6(List<Person> listP){
+        listP.stream()
+                .filter(x -> x.getAge() > 18)
+                .sorted(Comparator.comparing(Person::getAge))
+                .map(Person::getName)
+                .forEach(System.out::println);
+    }
+
+    // Превратить List<List<String>> приходящий на вход в строку, где каждая строка разделена запятой и пробелом.
+    public static void change7(List<List<String>> listCh){
+        String result = listCh.stream()
+                .flatMap(x -> x.stream())           // flatMap(List::stream) — превращаем список списков в плоский поток строк.
+                .collect(Collectors.joining(", "));
+        System.out.println(result);
+    }
+
+    //Ваша задача получить список Integer из массива int[]
+
+    //Ваша задача собрать Map из стрима чисел, где ключ это само число, а значение его квадрат. Сборка должна быть в LinkedHashMap
+
+    // Ваша задача взять первые 3 элемента из списка
+    public static void findFirstFreeAliment10(List<Integer> listF){
+        listF.stream().
+                limit(3).
+                forEach(System.out::println);
+    }
+
+    // Ваша задача сгруппировать пользователей по возрасту
 }
